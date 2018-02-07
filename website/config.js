6cedaa0b5235d1a4e8977bd00ada9e2fd9d1e2a1
Ran Ding <randing89@gmail.com>
2018-02-06 22:17:52 -0800

    Update library to node v9.5.0

--- website/config.js
+++ website/config.js
@@ -20,5 +20,9 @@ var themeCss = "themes/default-theme.css";
 
 var networkStat = {
     "bbs": [
+        ['pool.bbscoin.xyz', 'http://pool.bbscoin.xyz:8117'],
+        ['bbs.pool.pilbeams.net', 'http://bbs.pool.pilbeams.net:8111'],
+        ['bbs.pool.ahka.net', 'http://bbs.pool.ahka.net:8115'],
+        ['bbscoinpool.club', 'http://us.bbscoinpool.club:8117'],
     ]
 };
